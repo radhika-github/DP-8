3d2f22f6145a137f6a2be152b0ce524e4f96486d
Radhika <radhi.virgo.16@gmail.com>
2020-06-08 16:02:58 -0500

    Solution

---
+++ LongestIncreasingSubsequence.java
@@ -0,0 +1,24 @@
+// Time Complexity: O(n^2)
+// Space Complexity: O(n^2)
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        int max = 1;
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+
+        return max;
+    }
+}
